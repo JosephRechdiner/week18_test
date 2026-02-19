b1f30f328b0cf003e6033e97148b324a46b45f87
JosephRechdiner <jrechdiner2002@gmail.com>
2026-02-19 10:02:39 +0200

    added pydantic validation

--- producer/models.py
+++ producer/models.py
@@ -1,4 +1,12 @@
-from pydantic import BaseModel
+from typing import Literal
+from pydantic import BaseModel, Field
 
 class Alert(BaseModel):
-    pass
+    border: Literal["egypt", "lebanon", "gaza", "syria", "jordan"]
+    zone: str
+    timestamp: str
+    people_count: int
+    weapons_count: int
+    vehicle_type:  Literal["motorcycle", "jeep", "truck", "car", None]
+    distance_from_fence_m: int
+    visibility_quality: int | float = Field(gt=0, lt=100)
